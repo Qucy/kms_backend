4a382817dc915af403e923f33170f9ed0634cb30
Qucy <tracyqucy@gmail.com>
2022-08-03 23:21:17 +0800

    update notebook

--- sqlite.sql
+++ sqlite.sql
@@ -2,3 +2,7 @@
 SELECT * from image_image;
 
 select * from image_tag;
+
+--insert into image_imagetaglinkage values (1, 3, 1, 'Qucy', '2022-08-02 22:00:00');
+
+select * from image_imagetaglinkage;
